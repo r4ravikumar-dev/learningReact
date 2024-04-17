7db76f6850ebc3eb567ba2ed6b2ce8102d4b632c
Ravi Kumar <rravikumar27316@gmail.com>
2024-04-17 21:05:38 +0530

    changes made in the custom react folder and added other version of the same

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,21 +1,34 @@
 function customRender(reactElement, container){
+    /* version: 01
+    
     const domElement = document.createElement(reactElement.type)
     domElement.innerHTML = reactElement.children
     domElement.setAttribute('href', reactElement.props.href)
     domElement.setAttribute('target', reactElement.props.target)
 
     container.appendChild(domElement)
+    */
+
+    // version: 02
+
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    for(const prop in reactElement.props) {
+        if (prop === 'children') continue;
+        domElement.setAttribute(prop, reactElement.props [prop])
+    }
+    container.appendChild(domElement)
 }
 
 const reactElement = {
-    type: 'a',
+    type: 'a', // defines the type of the element
     props: {
         href: 'https://reactjs.org',
         target: '_blank',
-    },
+    }, // props defines the properties of the element and it is of object type
     children: 'Click me to visit this page'
 }
 
 const mainContainer = document.querySelector('#root')
 
-customRender(reactElement, mainContainer)
+customRender(reactElement, mainContainer) // renders the react element and it is of function type
