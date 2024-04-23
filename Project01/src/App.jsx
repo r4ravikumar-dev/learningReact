22ffffe4f9968e2196f342a2a15fd7e2ed0fca48
Ravi Kumar <rravikumar27316@gmail.com>
2024-04-23 22:13:23 +0530

    made my first project

--- Project01/src/App.jsx
+++ Project01/src/App.jsx
@@ -8,15 +8,21 @@ function App() {
   const addValue = () => {
     if (counter < 25)
     setCounter(counter+1)
-    else
+    else {
       counter = 25
+      alert("The value of the counter can't be more than 25");
+    }
+      
   }
 
   const removeValue = () => {
     if (counter > 0)
       setCounter(counter-1)
-    else
+    else{
       counter = 0
+      alert("The value of the counter can't be negative");
+    }
+      
   }
   
   const resetValue =() => {
@@ -26,16 +32,17 @@ function App() {
   return (
     <div className="min-h-screen w-full flex flex-wrap flex-col justify-center bg-gradient-to-r from-violet-700 to to-fuchsia-900 ">
       <div className="flex flex-wrap items-center justify-center text-center ">
-        <h1 className="text-5xl font-bold text-blue-400">Chai aur React</h1>
+        <h1 className="text-4xl font-bold text-blue-400">Chai aur React</h1>
       </div>
       <div className="flex flex-wrap items-center justify-center text-center mt-4 mb-8 ">
-        <h1 className="text-7xl font-bold text-yellow-600 border-b-4 border-yellow-600">Number counter</h1>
+        <h1 className="text-6xl font-bold text-yellow-400 border-b-4 border-yellow-500">Number counter</h1>
       </div>
       <div className="flex flex-wrap flex-col items-center justify-center my-4 gap-4">
-        <div className="flex flex-wrap items-center justify-center text-center">
-          <h2 className="text-white font-semibold text-2xl">Counter Value: {counter}</h2>
+        <div className="flex flex-wrap flex-col items-center justify-center text-center">
+          <h2 className="text-white font-semibold text-2xl mb-4">Counter Value:</h2>
+          <h1 className="text-red-600 font-bold text-6xl  p-4 border-4 rounded-2xl border-red-600">{counter}</h1>
         </div>
-        <div className="flex flex-wrap items-center justify-center text-center gap-2">
+        <div className="flex flex-wrap items-center justify-center text-center gap-2 mt-16">
           <button className="text-lg font-semibold text-blue-700 px-4 border-2 border-blue-800 rounded-md bg-blue-300 hover:bg-blue-700 hover:text-white" onClick={addValue}> add value </button>
           <button className="text-lg font-semibold text-red-700 px-4 border-2 border-red-800 rounded-md bg-red-300 hover:bg-red-700 hover:text-white " onClick={removeValue}> remove value </button>
         </div>
